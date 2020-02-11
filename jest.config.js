ca44c21e53a9933e2a7ede2410f1b76cf7c22994
Jasper Schreel <jasperschreel@gmail.com>
2020-02-11 13:52:48 +0100

    script test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  testEnvironment: 'node',
+    preset: 'ts-jest/presets/js-with-ts',
+    testEnvironment: 'node',
+    testMatch: [
+        "**/?(*.)+(spec|test).[t]s?(x)"
+    ]
 };
