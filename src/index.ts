806ed90b11c5e8507070bb2fdff05c93c5bdee13
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2020-03-16 11:03:09 +0100

    fix on index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import {keypairFromAccount, revineAddressFromSeed} from "./service/cryptoService";
+import {calculateWalletEntropyFromAccount, keypairFromAccount, revineAddressFromSeed} from "./service/cryptoService";
 import {Keypair} from "stellar-sdk";
 import {generateAccount} from "./service/stellarService";
 
 export const convertTfAccount: (seedPhrase: string, walletAmount?: number, startIndex?: number) => Promise<void> = async (seedPhrase: string, walletAmount: number = 1, startIndex: number = 1) => {
     for (let i = 0; i < walletAmount; i++) {
-        const stellarPair: Keypair = keypairFromAccount(seedPhrase, startIndex + i);
+        const walletEntropy = calculateWalletEntropyFromAccount(seedPhrase, startIndex + i);
+        const stellarPair = keypairFromAccount(walletEntropy);
+
         const revinePair = revineAddressFromSeed(seedPhrase, startIndex + i)
         await generateAccount(stellarPair, revinePair);
     }
