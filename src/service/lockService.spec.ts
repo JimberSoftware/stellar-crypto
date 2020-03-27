276cedd303c267a95195c629fa8096f3b46cb668
Jonas Wijne <jonas.wijne@jimber.org>
2020-03-27 10:21:42 +0100

    add initial unlockl functionality

--- src/service/lockService.spec.ts
+++ src/service/lockService.spec.ts
@@ -1,17 +1,49 @@
 import {calculateWalletEntropyFromAccount, keypairFromAccount, revineAddressFromSeed} from "./cryptoService";
-import {getLockedBalances} from "./lockService";
+import {fetchUnlockTransaction, getLockedBalances} from "./lockService";
+import {getConfig} from "./stellarService";
 
 
-const seedPhrase: string = "enlist extend limb diet crucial broccoli inhale trick stuff sting talent runway announce surprise dog limb second sun april reason they produce search slab";
+const seedPhrase: string = "enlist extend long diet crucial broccoli inhale tuna stuff sting miracle runway announce surprise dog limb second sun april reason they produce sick spray";
 
 const walletEntropy = calculateWalletEntropyFromAccount(seedPhrase, 0);
 const keypair = keypairFromAccount(walletEntropy);
-console.log(revineAddressFromSeed(seedPhrase, 0))
+console.log(keypair.publicKey())
 
 describe('lockservice', () => {
     it('should have locked tokens', async () => {
         const lockedFunds = await getLockedBalances(keypair);
 
         console.log(lockedFunds);
+        expect(lockedFunds).toStrictEqual([
+            {
+                id: 'GBS5U2ZBW6BO6JVZW72RUMLZFKRMILDPA7VE3DV7LYVYJHCOKP7JJ4HV',
+                balance: '50.0000000',
+                unlockHash: 'TAEYUR6Y2O2IQE4PXMIBGTTNUSLNBSQ3NGO4C5Z3RO4INHKYU6UUDPSY'
+            }
+        ])
+    });
+    it('should fetch unlockTransactionEnvelope', async () => {
+        const [lockedFunds, ...rest] = await getLockedBalances(keypair);
+        const unlockTransactionEnvelope = await fetchUnlockTransaction(lockedFunds.unlockHash);
+        expect(unlockTransactionEnvelope.source).toEqual(lockedFunds.id)
+    });
+    it.skip('should unlock escrow account', async () => {
+        const [lockedFunds, ...rest] = await getLockedBalances(keypair);
+        const unlockTransaction = await fetchUnlockTransaction(lockedFunds.unlockHash);
+
+        const { server } = getConfig();
+
+        try {
+            const result = await server.submitTransaction(unlockTransaction);
+            console.log('Success! Results:', result);
+
+        } catch (error) {
+            console.error('Something went wrong!', error.response.data);
+            // If the result is unknown (no response body, timeout etc.) we simply resubmit
+            // already built transaction:
+            // await server.submitTransaction(fundedTransaction);
+        }
+
     });
 });
+
--- src/service/lockService.ts
+++ src/service/lockService.ts
@@ -1,18 +1,35 @@
 import {getConfig} from "./stellarService";
-import {Keypair} from "stellar-sdk";
+import {Keypair, Transaction} from "stellar-sdk";
+import axios from 'axios';
 
-export const getLockedBalances = async (keypair: Keypair) => {
+
+export const fetchUnlockTransaction = async (unlockHash: string) => {
+    const {serviceUrl} = getConfig();
+    const result = await axios.post(`${serviceUrl}/unlock_service/get_unlockhash_transaction`,
+        {
+            args: {unlockhash: unlockHash}
+        }
+    );
+
+    //@todo: validation
+    const {network} = getConfig();
+    return new Transaction(result.data.transaction_xdr, network);
+
+};
+
+export const getLockedBalances = async (keyPair: Keypair) => {
     const {server} = getConfig();
 
-    const accounts = server.accounts().forSigner(keypair.publicKey());
+    const accounts = server.accounts().forSigner(keyPair.publicKey());
 
     const accountRecord = await accounts.call();
     return accountRecord.records
-        .filter(a => a.id !== keypair.publicKey())
+        .filter(a => a.id !== keyPair.publicKey())
         .map(account => {
             return {
                 id: account.id,
-                balance: account.balances.find(b => b.asset_type === 'credit_alphanum4' && b.asset_code === 'TFT').balance
+                balance: account.balances.find(b => b.asset_type === 'credit_alphanum4' && b.asset_code === 'TFT').balance,
+                unlockHash: account.signers.find(s => s.type === 'preauth_tx').key,
             }
         });
 };
