cf18224619ca76471ea3087a02a4f1afe9b352c0
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-04-12 16:34:42 +0200

    remove unused function

--- src/service/bscService.ts
+++ src/service/bscService.ts
@@ -7,12 +7,3 @@ export function convertBep20ToStellarMemo(address: string): string {
 
     return Buffer.from(address.replace('0x', ''), 'hex').toString('base64')
 }
-
-export function hex2a (hex: string): string {
-    var str = ''
-    for (var i = 0; i < hex.length; i += 2) {
-        var v = parseInt(hex.substr(i, 2), 16)
-        if (v) str += String.fromCharCode(v)
-    }
-    return str
-}
